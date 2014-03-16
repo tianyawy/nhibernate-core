11cfbeaf870eca7347e5fcf9520bc67c8ce37952
tianya_wy <tianya_wy@hotmail.com>
2014-03-16 20:54:09 +0800

    add new function

---
+++ src/NHibernate.TestDatabaseSetup/TestDatabaseExport.cs
@@ -0,0 +1,12 @@
+﻿using NHibernate.Tool.hbm2ddl;
+
+namespace NHibernate.TestDatabaseSetup
+{
+    public class TestDatabaseExport
+    {
+        public  SchemaExport GetSchemaExport()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/NHibernate.TestDatabaseSetup/TestDatabaseExportTest.cs
@@ -0,0 +1,19 @@
+﻿using Moq;
+using NHibernate.Tool.hbm2ddl;
+using NUnit.Framework;
+
+namespace NHibernate.TestDatabaseSetup
+{
+    [TestFixture]
+    public class TestDatabaseExportTest
+    {
+        [Test]
+        public void shouldCallSchemaExportCreateWithConfiguration()
+        {
+            var mock = new Mock<TestDatabaseExport>();
+            var calls = 0;
+            mock.Setup(export => export.GetSchemaExport()).Callback(() => calls++);
+            
+        }
+    }
+}
